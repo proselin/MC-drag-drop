a646d4f178ec379b23e14ae815339a003189634c
Nguyễn Quốc Hưng <72499558+proselin@users.noreply.github.com>
2022-06-19 09:22:46 +0700

    Intital commit

---
+++ main.js
@@ -0,0 +1,52 @@
+window.onload = function() {
+	var canvas = document.getElementById("canvas"),
+		context = canvas.getContext("2d"),
+		width = canvas.width = window.innerWidth *0.9,
+		height = canvas.height = window.innerHeight *0.9,
+		handle = {
+			x: 0,
+			y: 0,
+			radius: 30,
+			edge: 100,
+			height: 100,
+			width:100
+		},
+		offset = {};
+
+	draw();
+
+	function draw() {
+		context.clearRect(0, 0, width, height);
+
+		context.fillStyle = "blue";
+		context.beginPath();
+		// context.arc(handle.x, handle.y,10, 0, Math.PI * 2, false);
+		context.rect(handle.x, handle.y,handle.edge,handle.edge)
+		context.fill();
+	}
+
+	document.body.addEventListener("mousedown", function(event) {
+		console.log(utils.rectPointCollision(event.clientX, event.clientY, handle));
+		if(utils.pointInRect(event.clientX, event.clientY, handle)) {
+			document.body.addEventListener("mousemove", onMouseMove);
+			document.body.addEventListener("mouseup", onMouseUp);
+			console.log(handle.x,  handle.y);
+			console.log(event.clientX,  event.clientY);
+			offset.x = (event.clientX - handle.x)/2;
+			offset.y = (event.clientY - handle.y)/2;
+		}
+	});
+
+	function onMouseMove(event) {
+		handle.x = event.clientX - offset.x;
+		handle.y = event.clientY - offset.y;
+		draw();
+	}
+
+	function onMouseUp(event) {
+		document.body.removeEventListener("mousemove", onMouseMove);
+		document.body.removeEventListener("mouseup", onMouseUp);
+	}
+
+
+};
